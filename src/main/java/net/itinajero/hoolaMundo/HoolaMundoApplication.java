c8643110cb5b6c6a2e45bd4db4277277ac258600
Santiago <santiago.pechalamilla@cbtis72.edu.mx>
2023-11-18 19:18:31 -0500

    commit de ejemplo

---
+++ src/main/java/net/itinajero/hoolaMundo/HoolaMundoApplication.java
@@ -0,0 +1,13 @@
+package net.itinajero.hoolaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoolaMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/itinajero/hoolaMundo/HoolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.itinajero.hoolaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
